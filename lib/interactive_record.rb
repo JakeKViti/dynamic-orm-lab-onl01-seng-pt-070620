76715a032fdc27d516d959ca321deea97a8c466a
JakeKViti <thevitij64@gmail.com>
2020-09-11 20:16:00 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,9 +55,7 @@ class InteractiveRecord
     end
 
     def self.find_by(attribute)
-        atter1 = attribute.values[0]
-
       sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys[0]} = ?"
-      DB[:conn].execute(sql, atter1)
+      DB[:conn].execute(sql, attribute.values[0])
     end
 end
